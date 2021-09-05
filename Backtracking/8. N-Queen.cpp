3044d9afb09fdf622c1633624667a929e34a37b4
its_shyam640 <shyamvashishtha640@gmail.com>
2021-09-05 22:25:52 +0530

    Added New Backtracking Problems

--- Backtracking/8. N-Queen.cpp
+++ Backtracking/8. N-Queen.cpp
@@ -1,6 +1,9 @@
 // Question Link --> https://leetcode.com/problems/n-queens
+// Question Link --> https://leetcode.com/problems/n-queens-ii/
 
 
+
+// <===================   Placing Queens   ==========================>
 class Solution {
 public:
     bool isItSafe(vector<string>& grid,int n,int row,int col){
@@ -65,5 +68,72 @@ public:
         vector<string> grid(n,string(n,'.'));
         placeQueens(ans,grid,n,0);
         return ans;
+    }
+};
+
+
+
+
+
+// <===================== Returing total number of ways to place queens ======================>
+class Solution {
+public:
+    bool isItSafe(vector<vector<bool>>& grid,int n,int row,int col){
+        for(int i=0;i<n;i++)
+            if(grid[row][i] and i!=col) return false;
+        
+        for(int i=0;i<n;i++)
+            if(grid[i][col] and i!=row) return false;
+        
+        int i=row,j=col;
+        while(i<n and i>=0 and j<n and j>=0){
+            if(grid[i][j] and i!=row and j!=col)  return false;
+            i++;
+            j++;
+        }
+        
+        i=row,j=col;
+        while(i<n and i>=0 and j<n and j>=0){
+            if(grid[i][j] and i!=row and j!=col) return false;
+            i--;
+            j--;
+        }
+        
+        i=row,j=col;
+        while(i<n and i>=0 and j<n and j>=0){
+            if(grid[i][j] and i!=row and j!=col) return false;
+            i--;
+            j++;
+        }
+        
+        i=row,j=col;
+        while(i<n and i>=0 and j<n and j>=0){
+            if(grid[i][j] and i!=row and j!=col) return false;
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+    
+    int totalQueens=0;
+    
+    void placeQueens(vector<vector<bool>>& grid,int n,int row){
+        if(row==n){
+            totalQueens++;
+            return;
+        }
+        for(int i=0;i<n;i++){
+            grid[row][i] = true;
+            if(isItSafe(grid,n,row,i))
+                placeQueens(grid,n,row+1);
+            grid[row][i] = false;
+        }
+    }
+    
+    int totalNQueens(int n) {
+        vector<vector<bool>> grid(n,vector<bool>(n,false));
+        placeQueens(grid,n,0);
+        return totalQueens;
     }
 };
