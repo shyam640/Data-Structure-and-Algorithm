88307646fad7264bab4595f6e47f8df8dee025d0
shyam640 <shyamvashishtha640@gmail.com>
2021-10-24 14:13:13 +0530

    Updated Repository

--- Sorting Algorithms/1. Selection Sort.cpp
+++ Sorting Algorithms/1. Selection Sort.cpp
@@ -2,7 +2,12 @@
 using namespace std;
 
 void sortArr(vector<int>& arr){
-   
+   for(int i=0;i<arr.size()-1;i++){
+      for(int j=i+1;j<arr.size();j++){
+         if(arr[j]<arr[i])
+            swap(arr[i],arr[j]);
+      }
+   }
 }
 
 int main(){
---
+++ my_cpp_snippet.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define               li                long int
+#define               lf                long float
+#define               ld                long double
+#define               lli               long long int
+#define               llf               long long float
+#define               lld               long long double
+#define               mod               1000000007
+#define               infi              1e18
+#define               endl              "\n"
+#define               pf                push_front
+#define               pb                push_back
+#define               ob                pop_back
+#define               of                pop_front
+#define               vi                vector<int>
+#define               vc                vector<char>
+#define               vs                vector<string>
+#define               vli               vector<long int> 
+#define               vlf               vector<long float>
+#define 
