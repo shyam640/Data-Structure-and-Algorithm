de998face94f37522ee424544603d7fb97ebf3a7
shyam640 <shyamvashishtha640@gmail.com>
2022-04-19 14:41:12 +0530

    Few New Problems Added

---
+++ test.java
@@ -0,0 +1,11 @@
+class Test {
+   public static void main(String ... args) {
+      Set s = new TreeSet();
+      s.add(25);
+      s.add(3);
+      s.add(67);
+      s.add(54);
+      s.add(34);
+      s.add(12);
+   }
+}
