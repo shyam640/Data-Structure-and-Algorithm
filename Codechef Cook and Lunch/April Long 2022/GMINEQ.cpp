6d171187f96a61243b46f89903a447a58e48801d
shyam640 <shyamvashishtha640@gmail.com>
2022-04-27 12:56:35 +0530

    Solved New Problems

---
+++ Codechef Cook and Lunch/April Long 2022/GMINEQ.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define               li                        long int
+#define               lf                        long float
+#define               ld                        long double
+#define               lli                       long long int
+#define               llf                       long long float
+#define               lld                       long long double
+#define               usig                      unsigned
+#define               sig                       signed
+#define               mod                       1000000007
+#define               infi                      1e18
+#define               neg_infi                  -1e18
+#define               endl                      "\n"
+#define               vi                        vector<int>
+#define               vc                        vector<char>
+#define               vs                        vector<string>
+#define               vli                       vector<long int> 
+#define               vlf                       vector<long float>
+#define               vld                       vector<long double>
+#define               vlli                      vector<long long int> 
+#define               vllf                      vector<long long float>
+#define               vlld                      vector<long long double>
+
+#define               pf                        push_front
+#define               pb                        push_back
+#define               ob                        pop_back
+#define               of                        pop_front
+#define               pq                        priority_queue
+#define               ump                       unordered_map
+
+
+#define               it(var)                   var.begin(),var.end()
+#define               loop(type,var,st,end)     for(type var=st;var<end;var++)
+#define               loop_rev(type,var,st,end) for(type var=st;var<end;var++)
+#define               mid(left,right)           (left+(right-left)/2)
+#define               print_arr(arr,type,st,end) for(type var = st;var<=end;var++)  cout<<arr[var]<<" "; cout<<endl;
+
+
+template <typename T> T _gcd(T a, T b){if(a%b) return _gcd(b,a%b);return b;}
+template <typename T> T _lcm(T a, T b){return (a*(b/_gcd(a,b)));}
+#define              fast_io                    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+const long double pi = atan2(0, -1);
+
+
+int main() {
+   fast_io
+   // Write your code here....
+   lli t;
+   cin>>t;
+   while(t--){
+      lli n;
+      cin>>n;
+      lli pos=0,neg=0;
+      for(lli i=0;i<n;i++){
+         lli a;
+         cin>>a;
+         if(a==-1)
+            neg++;
+         else
+            pos++;
+      }
+      if(abs(pos-neg)<2)
+         cout<<"YES"<<endl;
+      else if(abs(pos-neg)==2){
+         if(neg%2)
+            cout<<"NO"<<endl;
+         else
+            cout<<"YES"<<endl;
+      }
+      else
+         cout<<"NO"<<endl;
+   }
+   return 0;
+}
---
+++ Codechef Practice Problems/1200-1400/CHBLLNS.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define               li                        long int
+#define               lf                        long float
+#define               ld                        long double
+#define               lli                       long long int
+#define               llf                       long long float
+#define               lld                       long long double
+#define               usig                      unsigned
+#define               sig                       signed
+#define               mod                       1000000007
+#define               infi                      1e18
+#define               neg_infi                  -1e18
+#define               endl                      "\n"
+#define               vi                        vector<int>
+#define               vc                        vector<char>
+#define               vs                        vector<string>
+#define               vli                       vector<long int> 
+#define               vlf                       vector<long float>
+#define               vld                       vector<long double>
+#define               vlli                      vector<long long int> 
+#define               vllf                      vector<long long float>
+#define               vlld                      vector<long long double>
+
+#define               pf                        push_front
+#define               pb                        push_back
+#define               ob                        pop_back
+#define               of                        pop_front
+#define               pq                        priority_queue
+#define               ump                       unordered_map
+
+
+#define               it(var)                   var.begin(),var.end()
+#define               loop(type,var,st,end)     for(type var=st;var<end;var++)
+#define               loop_rev(type,var,st,end) for(type var=st;var<end;var++)
+#define               mid(left,right)           (left+(right-left)/2)
+#define               print_arr(arr,type,st,end) for(type var = st;var<=end;var++)  cout<<arr[var]<<" "; cout<<endl;
+
+
+template <typename T> T _gcd(T a, T b){if(a%b) return _gcd(b,a%b);return b;}
+template <typename T> T _lcm(T a, T b){return (a*(b/_gcd(a,b)));}
+#define              fast_io                    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+const long double pi = atan2(0, -1);
+
+
+int main() {
+   fast_io
+   // Write your code here....
+   lli t;
+   cin>>t;
+   while(t--){
+      lli r,g,b;
+      cin>>r>>g>>b;
+      lli k;
+      cin>>k;
+      lli ans=0;
+      ans += (min(r,k-1) + min(g,k-1) + min(b,k-1) + 1);
+      cout<<ans<<endl;
+   }
+
+   return 0;
+}
